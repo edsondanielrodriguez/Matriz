cae9757b610f41aaef5b97331e32d159140a321e
edsondanielrodriguez <AZ.23238800@Gmail.com>
2024-02-25 21:28:28 -0600

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -8,8 +8,7 @@ public class Matriz{
     public static void main(String args[]){
         //variables
         int opcion = 0;
-        int[][] matriz;
-        int columna=0, renglon=0;
+    
 
         //Creación de objetos
         Scanner entrada = new Scanner(System.in);
@@ -27,30 +26,43 @@ public class Matriz{
 
         switch (opcion){
             case 1:
-                
+                int columna=0, renglon=0;
                 System.out.printf("%n");
-                
+                Scanner sc =  new Scanner(System.in);
+
                 do{
                     System.out.printf("Ingrese la cantidad de columnas: ");
-                    columna = entrada.nextInt();
+                    columna = sc.nextInt();
                 }while(columna<1);
                 
                 System.out.printf("%n");
                 do{
                     System.out.printf("Ingrese la cantidad de renglones: ");
-                    renglon = entrada.nextInt();
+                    renglon = sc.nextInt();
                 }while(renglon<1);
 
-                
+                /* 
                 Solicitud_Matriz matrix = new Solicitud_Matriz();
                 matrix.get_matriz(columna,renglon);
                 
+                /* 
                 //Mostrar datos de renglon
                 System.out.printf("%n");
                 matrix.mostrarColumnaRenglon();
                 System.out.printf("%n");
+                */
 
+                //Hacer matriz
+            
+                int matriz[][] = new int [renglon][columna];  
+                for(int i = 0; i < matriz.length; i++){
+                    for(int j = 0; j < matriz[i].length; j++){
+                        System.out.printf("Ingresa el nombre de la fila " + i + " y la columna " + j);
+                        matriz[i][j] = sc.next();
+                        System.out.printf("=)");
 
+                    }
+                }
 
                 break;
         
